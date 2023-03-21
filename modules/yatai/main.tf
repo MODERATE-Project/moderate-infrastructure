89fa9c3192c0c7afbfd9b35ead97042de5e22f92
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-03-21 18:02:38 +0100

    Add yatai-deployment

--- modules/yatai/main.tf
+++ modules/yatai/main.tf
@@ -55,20 +55,81 @@ resource "google_sql_database" "sql_database" {
 }
 
 locals {
-  ingress_config = {
+  yatai_wait_duration = "20s"
+}
+
+resource "helm_release" "yatai_deployment_crds" {
+  name       = "yatai-deployment-crds"
+  repository = "https://bentoml.github.io/helm-charts"
+  chart      = "yatai-deployment-crds"
+  version    = "1.1.7"
+  namespace  = local.namespace
+}
+
+resource "time_sleep" "wait_yatai_deployment_crds" {
+  depends_on      = [helm_release.yatai_deployment_crds]
+  create_duration = local.yatai_wait_duration
+}
+
+locals {
+  yatai_deployment_config = {
+    "layers" = {
+      "network" = {
+        "ingressClass" = "nginx"
+        "ingressAnnotations" : {
+          "cert-manager.io/cluster-issuer" = var.cert_manager_issuer
+        }
+      }
+    }
+    "yataiSystem" = {
+      "namespace" = local.namespace
+    }
+  }
+}
+
+resource "helm_release" "yatai_deployment" {
+  depends_on = [time_sleep.wait_yatai_deployment_crds]
+  name       = "yatai-deployment"
+  repository = "https://bentoml.github.io/helm-charts"
+  chart      = "yatai-deployment"
+  version    = "1.1.7"
+  namespace  = local.namespace
+  values     = [yamlencode(local.yatai_deployment_config)]
+}
+
+resource "time_sleep" "wait_yatai_deployment" {
+  depends_on      = [helm_release.yatai_deployment]
+  create_duration = local.yatai_wait_duration
+}
+
+locals {
+  yatai_config = {
+    "postgresql" = {
+      "host"     = var.postgres_host
+      "port"     = var.postgres_port
+      "user"     = google_sql_user.sql_user.name
+      "database" = google_sql_database.sql_database.name
+      "password" = google_sql_user.sql_user.password
+      "sslmode"  = "disable"
+    }
+    "s3" = {
+      "endpoint"   = "https://storage.googleapis.com"
+      "region"     = "auto"
+      "bucketName" = module.bucket.name
+      "secure"     = true
+      "accessKey"  = google_storage_hmac_key.bucket_hmac_key.access_id
+      "secretKey"  = google_storage_hmac_key.bucket_hmac_key.secret
+    }
     "ingress" = {
       "enabled"   = true
       "className" = "nginx"
-
       "annotations" = {
         "cert-manager.io/cluster-issuer" = var.cert_manager_issuer
       }
-
       "tls" = [{
         "secretName" = "yatai-ingress-tls-secret"
         "hosts"      = [var.domain]
       }]
-
       "hosts" = [{
         "host"  = var.domain
         "paths" = ["/"]
@@ -78,71 +139,11 @@ locals {
 }
 
 resource "helm_release" "yatai" {
+  depends_on = [time_sleep.wait_yatai_deployment]
   name       = "yatai"
   repository = "https://bentoml.github.io/helm-charts"
   chart      = "yatai"
   version    = "1.1.7"
   namespace  = local.namespace
-
-  set {
-    name  = "postgresql.host"
-    value = var.postgres_host
-  }
-
-  set {
-    name  = "postgresql.port"
-    value = var.postgres_port
-  }
-
-  set {
-    name  = "postgresql.user"
-    value = google_sql_user.sql_user.name
-  }
-
-  set {
-    name  = "postgresql.database"
-    value = google_sql_database.sql_database.name
-  }
-
-  set {
-    name  = "postgresql.password"
-    value = google_sql_user.sql_user.password
-  }
-
-  set {
-    name  = "postgresql.sslmode"
-    value = "disable"
-  }
-
-  set {
-    name  = "s3.endpoint"
-    value = "https://storage.googleapis.com"
-  }
-
-  set {
-    name  = "s3.region"
-    value = "auto"
-  }
-
-  set {
-    name  = "s3.bucketName"
-    value = module.bucket.name
-  }
-
-  set {
-    name  = "s3.secure"
-    value = true
-  }
-
-  set {
-    name  = "s3.accessKey"
-    value = google_storage_hmac_key.bucket_hmac_key.access_id
-  }
-
-  set {
-    name  = "s3.secretKey"
-    value = google_storage_hmac_key.bucket_hmac_key.secret
-  }
-
-  values = [yamlencode(local.ingress_config)]
+  values     = [yamlencode(local.yatai_config)]
 }
