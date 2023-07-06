29caf1f954dc3874a13a90e1b62deae6902d5734
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-07-06 11:55:15 +0200

    🩹 Add a suffix to the pool name

--- modules/artifact_registry/github_oidc.tf
+++ modules/artifact_registry/github_oidc.tf
@@ -13,10 +13,20 @@ resource "google_project_iam_member" "project" {
   member  = "serviceAccount:${google_service_account.sa.email}"
 }
 
+# Workload Identity Pool names cannot be reused, so we generate a random suffix
+# https://cloud.google.com/iam/docs/manage-workload-identity-pools-providers#delete-provider
+resource "random_string" "workload_identity_pool_suffix" {
+  length  = 4
+  special = false
+  lower   = true
+  upper   = false
+  numeric = false
+}
+
 module "gh_oidc" {
   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
   project_id  = var.project_id
-  pool_id     = "gh-artifact-registry-pool"
+  pool_id     = "gh-artifact-reg-pool-${random_string.workload_identity_pool_suffix.result}"
   provider_id = "gh-arfifact-registry-provider"
   sa_mapping = {
     (google_service_account.sa.account_id) = {
