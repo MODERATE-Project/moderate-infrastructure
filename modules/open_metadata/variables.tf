93a0b398cfc4e09bfb121084c152c98533912b4b
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2024-02-05 17:58:13 +0100

    ⬆️ Update open_metadata_chart_version to 1.2.8

--- modules/open_metadata/variables.tf
+++ modules/open_metadata/variables.tf
@@ -6,7 +6,7 @@ variable "namespace" {
 
 variable "open_metadata_chart_version" {
   type        = string
-  default     = "1.2.7"
+  default     = "1.2.8"
   description = "Open Metadata Helm chart version"
 }
 
