9373d37651af3cabd0e21ed4ee4b195bf83febca
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2024-06-04 12:42:19 +0200

    ⬆️ Upgrade cert-manager

--- modules/cert_manager/variables.tf
+++ modules/cert_manager/variables.tf
@@ -5,7 +5,7 @@ variable "after_install_crds_sleep" {
 
 variable "cert_manager_chart_version" {
   type    = string
-  default = "1.13.0"
+  default = "1.14.5"
 }
 
 variable "cluster_admin_account" {
