e7e2eed1220e084d300faf87335f2b2e7024421d
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2025-03-12 16:09:06 +0100

    🔧 Update default machine type for staging instance

--- modules/dev_compute_instance/variables.tf
+++ modules/dev_compute_instance/variables.tf
@@ -7,7 +7,7 @@ variable "instance_name" {
 variable "machine_type" {
   description = "Machine type for the instance"
   type        = string
-  default     = "e2-standard-4"
+  default     = "e2-standard-2"
 }
 
 variable "zone" {
